81b890f93ddc2a9bbfd4e5aa4f737ac833315f09
Minh Vuong <dmvuong95@gmail.com>
2021-01-20 17:51:53 +0700

    update auto reconnect

--- skyamqp/amqp_client.py
+++ skyamqp/amqp_client.py
@@ -4,6 +4,8 @@ from skyamqp.queue import Queue_Server_Thread, Queue_Client_Thread
 import threading
 import uuid
 import time
+import logging
+logger = logging.getLogger('skyamqp')
 
 class AMQP_Client:
   __queueCmd__ = []
@@ -67,6 +69,7 @@ class AMQP_Client:
     rpcClientList = {}
     queueServerList = {}
     queueClientList = {}
+    listCmd = {}
     while True:
       # print(1)
       if len(self.__queueCmd__):
@@ -76,12 +79,15 @@ class AMQP_Client:
         if cmd['method'] == 'create_RPC_Server':
           rpcServer = connection.create_RPC_Server(cmd['args']['queue'], cmd['args']['on_message'], cmd['args']['prefetch_count'])
           rpcServerList[cmd['args']['id']] = rpcServer
+          listCmd[cmd['args']['id']] = cmd
         if cmd['method'] == 'RPC_Server.stop':
           rpcServerList[cmd['args']['id']].stop()
+          listCmd[cmd['args']['id']] = None
         
         if cmd['method'] == 'create_RPC_Client':
           rpcClient = connection.create_RPC_Client(cmd['args']['queue'], cmd['args']['timeout'])
           rpcClientList[cmd['args']['id']] = rpcClient
+          listCmd[cmd['args']['id']] = cmd
         if cmd['method'] == 'RPC_Client.send':
           rpcClientList[cmd['args']['id']].send(cmd['args'])
 
@@ -89,34 +95,56 @@ class AMQP_Client:
         if cmd['method'] == 'create_Queue_Server':
           queueServer = connection.create_Queue_Server(cmd['args']['queue'], cmd['args']['group'], cmd['args']['on_message'], cmd['args']['prefetch_count'])
           queueServerList[cmd['args']['id']] = queueServer
+          listCmd[cmd['args']['id']] = cmd
         if cmd['method'] == 'Queue_Server.stop':
           queueServerList[cmd['args']['id']].stop()
+          listCmd[cmd['args']['id']] = None
 
         if cmd['method'] == 'create_Queue_Client':
           queueClient = connection.create_Queue_Client(cmd['args']['queue'])
           queueClientList[cmd['args']['id']] = queueClient
+          listCmd[cmd['args']['id']] = cmd
         if cmd['method'] == 'Queue_Client.send':
           queueClientList[cmd['args']['id']].send(cmd['args'])
         
         self.__queueCmd__.remove(cmd)
         pass
       else:
-        connection.__connection__.process_data_events(0.1)
+        try:
+          connection.__connection__.process_data_events(0.2)
+          pass
+        except pika.exceptions.StreamLostError as e:
+          logger.warning(e)
+          # re-connect
+          if connection.__connection__.is_open:
+            connection.__connection__.close()
+          time.sleep(0.2)
+          connection = AMQP_Client_Thread(host, username, password, virtual_host, port, heartbeat)
+          for key in listCmd:
+            if listCmd[key] is None:
+              pass
+            else:
+              self.__queueCmd__.append(listCmd[key])
+            pass
+          pass
         pass
 
 class AMQP_Client_Thread:
   __connection__: pika.BlockingConnection
   __clientChannel__: pika.adapters.blocking_connection.BlockingChannel
   def __init__(self, host, username, password, virtual_host: str = '/', port: int = 5672, heartbeat: int = 0):
-    self.__connection__ = pika.BlockingConnection(
-      pika.ConnectionParameters(
-        host=host,
-        port=port,
-        virtual_host=virtual_host,
-        heartbeat=heartbeat,
-        credentials=pika.PlainCredentials(username, password)
+    try:
+      self.__connection__ = pika.BlockingConnection(
+        pika.ConnectionParameters(
+          host=host,
+          port=port,
+          virtual_host=virtual_host,
+          heartbeat=heartbeat,
+          credentials=pika.PlainCredentials(username, password)
+        )
       )
-    )
+    except pika.exceptions.AMQPConnectionError as e:
+      raise e
   
   def create_RPC_Server(self, queue: str, on_message: None, prefetch_count: int):
     return RPC_Server_Thread(self.__connection__, queue, on_message, prefetch_count)
